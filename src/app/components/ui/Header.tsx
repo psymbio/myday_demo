8c80a5615b6da38931b6f3f2f003debe4b9b9edb
psymbio <psymbio@gmail.com>
2024-09-17 18:59:45 +0530

    header color change

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="border-b border-gray-300 bg-black shadow-sm">
+    <header className="border-b border-gray-300 bg-red-600 shadow-sm">
       <div className="mx-auto max-w-screen-xl px-4 py-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-start gap-2 md:flex-row md:items-center md:justify-between">
           <div>
