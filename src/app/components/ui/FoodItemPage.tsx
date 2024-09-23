8ca8510251efdb7e828601dd68f5b305d2d92069
TH <thazarika25@gmail.com>
2024-09-23 10:06:14 +0530

    Updated the restruant menu

--- src/app/components/ui/FoodItemPage.tsx
+++ src/app/components/ui/FoodItemPage.tsx
@@ -90,7 +90,7 @@ const RestaurantItem: React.FC<{
             onClick={handleFilter}
             className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition"
           >
-            View Menu
+            {restaurant.name} Menu
           </button>
         </div>
       </div>
