9ed93cf97473fab8301a443009693c018f6804e1
TH <thazarika25@gmail.com>
2024-10-01 09:03:07 +0530

    Update the HeaderFnD

--- src/app/foodcard/page.tsx
+++ src/app/foodcard/page.tsx
@@ -1,7 +1,7 @@
 
 
 
-import Header from '@/app/components/ui/Header';
+import Header from '@/app/components/ui/HeaderFnD';
 import NavigationBar from '@/app/components/ui/NavigationBar';
 import ChatBot from '@/app/components/ui/ChatBot';
 import HeaderImage from '@/app/components/ui/HeaderImageFnD';
