ec9b61e74b9f8c36f7dc429bc14f18def9df2fd0
psymbio <psymbio@gmail.com>
2024-10-01 14:53:05 +0530

    menu pref

--- src/app/components/ui/Modal.tsx
+++ src/app/components/ui/Modal.tsx
@@ -11,13 +11,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg shadow-lg p-8 relative w-full max-w-md">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-        >
-          ✖
-        </button>
+      <div className="bg-white rounded-lg shadow-lg p-8 relative w-10/12 max-w-md">
         {children}
       </div>
     </div>
