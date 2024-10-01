16daa95c2478957a0387245d2c533da4163b2a98
psymbio <psymbio@gmail.com>
2024-10-01 16:34:29 +0530

    header image'

--- src/app/components/ui/HeaderImage.tsx
+++ src/app/components/ui/HeaderImage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function HeaderImage() {
   return (
     <header className="flex justify-center w-full px-4">
-      <div className="relative h-48 md:h-64 lg:h-80 w-full max-w-2xl -translate-y-2"> {/* Use responsive heights */}
+      <div className="relative h-36  md:h-64 lg:h-80 w-full max-w-2xl -translate-y-2"> {/* Use responsive heights */}
         <Image
           src="/panorama_st_pauls.jpeg"
           alt="PSP"
