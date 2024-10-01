66810ce7ccf85d503cd7e70480296170074b3c61
psymbio <psymbio@gmail.com>
2024-10-01 12:13:57 +0530

    padding

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -13,7 +13,7 @@ export default function TestingPage() {
       <Header></Header>
       
       <HeaderImage></HeaderImage>
-      <div className="px-5 sm:px-5 mx-10 md:mx-16 lg:mx-32">
+      <div className="sm:px-2 mx-5 md:mx-16 lg:mx-32">
         <Tabs />
         <Carousel />
         <QuickLinks />
