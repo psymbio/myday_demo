d95e3994e3f6ed5a146085df5d4e41656ba1344c
sweetywajpe <sweety.wajpe@gmail.com>
2024-10-01 05:32:38 -0700

    button changes

--- src/app/components/ui/HomeToDeskpopup1.tsx
+++ src/app/components/ui/HomeToDeskpopup1.tsx
@@ -14,19 +14,19 @@ const HomeToDeskpopup1: React.FC<HomeToDeskpopup1Props> = ({ show, title, messag
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-lg font-semibold mb-4">{title}</h2>
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-10/12">
+        <h2 className="text-lg text-center font-semibold mb-4">{title}</h2>
         <p className="mb-6">{message}</p>
         <div className="flex justify-center space-x-4">
           <button
             onClick={onYes}
-            className="bg-white text-black border border-black hover:bg-gray font-semibold py-2 px-4 transition"
+            className="bg-white  text-black border border-black shadow-lg hover:bg-gray font-semibold py-2 px-10 transition"
           >
             Yes
           </button>
           <button
             onClick={onNo}
-            className="bg-white text-black border border-black hover:bg-gray  font-semibold py-2 px-4 transition"
+            className="bg-white text-black border border-black shadow-lg hover:bg-gray  font-semibold py-2 px-10 transition"
           >
             No
           </button>
