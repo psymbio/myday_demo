669d8db596ba3aa9de1fc0de392a77deceb84071
psymbio <psymbio@gmail.com>
2024-10-02 18:56:01 +0530

    header fix

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="border-b border-gray-300 bg-red-600 shadow-sm">
-      <div className="mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-8 pb-6">
         <div className="flex flex-col items-start gap-2 md:flex-row md:items-center md:justify-between">
           <div>
             <h1 className="text-base font-medium text-white sm:text-lg">
