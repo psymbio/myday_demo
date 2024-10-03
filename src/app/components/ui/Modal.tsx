8520e1b7fed55dc1e5b4c2ab3e30888c805f4b9b
TH <thazarika25@gmail.com>
2024-10-03 18:32:55 +0530

    Updated the FnD w10/12

--- src/app/components/ui/Modal.tsx
+++ src/app/components/ui/Modal.tsx
@@ -12,7 +12,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       {/* Modal content */}
-      <div className="bg-white rounded-lg shadow-lg p-8 relative w-full max-w-md">
+      <div className="bg-white rounded-lg shadow-lg p-8 relative w-10/12 max-w-md">
         {children}
       </div>
     </div>
