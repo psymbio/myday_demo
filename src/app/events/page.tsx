18728dce79ab196e5f683db2e91fa3090af35ac3
psymbio <psymbio@gmail.com>
2024-10-04 15:38:18 +0530

    event carousel css fix

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,14 +1,14 @@
 import ChatBot from "@/app/components/ui/ChatBot";
-import Header from "@/app/components/ui/Header";
 import HeaderImage from "@/app/components/ui/HeaderImage";
 import NavigationBar from "@/app/components/ui/NavigationBar";
 import EventCardDisplay from "@/app/components/ui/EventCardDisplay";
+import HeaderEvents from "../components/ui/HeaderEvents";
 
 export default function TestingPage() {
   return (
     <div>
       <NavigationBar></NavigationBar>
-      <Header></Header>
+      <HeaderEvents></HeaderEvents>
       <HeaderImage></HeaderImage>
       <EventCardDisplay></EventCardDisplay>
       <ChatBot></ChatBot>
