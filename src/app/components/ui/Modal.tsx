fee8d3d8c46db07306ec23bdbf75bb78979a1a30
TH <thazarika25@gmail.com>
2024-10-04 11:37:30 +0530

    Updated errors in FnD

--- src/app/components/ui/Modal.tsx
+++ src/app/components/ui/Modal.tsx
@@ -10,7 +10,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-30">
       {/* Modal content */}
       <div className="bg-white rounded-lg shadow-lg p-8 relative w-10/12 max-w-md">
         {children}
