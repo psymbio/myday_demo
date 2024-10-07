4245adea9d4292a4c3d4e26d87fa6b2d1ca6230c
psymbio <psymbio@gmail.com>
2024-10-07 21:24:37 +0530

    events header image

---
+++ src/app/components/ui/HeaderImageEvents.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function HeaderImage() {
+  return (
+    <header className="flex justify-center w-full px-4">
+      <div className="relative h-36  md:h-64 lg:h-80 w-full max-w-2xl -translate-y-4">
+        {" "}
+        {/* Use responsive heights */}
+        <Image
+          src="/events.jpg"
+          alt="PSP"
+          layout="fill"
+          objectFit="cover"
+          priority={true}
+        />
+      </div>
+    </header>
+  );
+}
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,15 +1,15 @@
 import ChatBot from "@/app/components/ui/ChatBot";
-import HeaderImage from "@/app/components/ui/HeaderImage";
 import NavigationBar from "@/app/components/ui/NavigationBar";
 import EventCardDisplay from "@/app/components/ui/EventCardDisplay";
 import HeaderEvents from "../components/ui/HeaderEvents";
+import HeaderImageEvents from "../components/ui/HeaderImageEvents";
 
 export default function TestingPage() {
   return (
     <div>
       <NavigationBar></NavigationBar>
       <HeaderEvents></HeaderEvents>
-      <HeaderImage></HeaderImage>
+      <HeaderImageEvents></HeaderImageEvents>
       <EventCardDisplay></EventCardDisplay>
       <ChatBot></ChatBot>
     </div>
