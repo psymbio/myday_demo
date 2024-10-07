b79e296159891ddae8766a2d2f1559c7d8b2e608
psymbio <psymbio@gmail.com>
2024-10-07 21:07:44 +0530

    filter button

--- src/app/components/ui/FoodDisplay.tsx
+++ src/app/components/ui/FoodDisplay.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useState } from 'react';
-import FoodCard from './FoodCard';
-import Modal from './Modal';
-import foodItems from '../../data/fooditems.json'; // Assuming fooditems.json is in this path
-import TeyaCard from './TeyaCard';
-import CustomButton from './CustomButton';
-import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
+import FoodCard from "./FoodCard";
+import Modal from "./Modal";
+import foodItems from "../../data/fooditems.json"; // Assuming fooditems.json is in this path
+import TeyaCard from "./TeyaCard";
+import CustomButton from "./CustomButton";
+import { useRouter } from "next/navigation";
+import FilterAltRoundedIcon from "@mui/icons-material/FilterAltRounded";
 
 interface FoodItem {
   id: number;
@@ -57,7 +58,6 @@ const FoodCardDisplay: React.FC = () => {
       glutenFree: String(tempFilter.glutenFree),
     }).toString();
 
-
     router.push(`/food_drink/filter?${query}`);
   };
 
@@ -80,126 +80,127 @@ const FoodCardDisplay: React.FC = () => {
   return (
     <div className="p-0 relative content-center font-bold">
       {/* Filter and Clear Selection Buttons Row */}
-      <div className="-mt-5 mb-4">
+      <div className="-mt-5 mb-4 flex items-center justify-center">
         {/* Full-width Clickable Filter Menu Row */}
-        <div
-          className="w-full bg-red-500 cursor-pointer hover:bg-gray-200 text-white font-bold flex items-center justify-center underline"
-          onClick={openModal} // Open modal when button is clicked
+        <button
+          className="w-10/12 p-3 bg-red-600 text-white font-bold flex items-center justify-center rounded-lg shadow-lg hover:bg-red-700 hover:text-white transition duration-300 ease-in-out cursor-pointer"
+          onClick={openModal}
         >
+          <FilterAltRoundedIcon className="mr-2" />
           FILTER
-        </div>
+        </button>
         {/* Teya Card Below Filter Menus */}
-        
       </div>
       <div className="flex justify-center -mt-4">
-  <TeyaCard />
-</div>
+        <TeyaCard />
+      </div>
 
       {/* Modal for filtering */}
-{isModalOpen && (
-  <Modal isOpen={isModalOpen} onClose={closeModal}>
-    {/* Semi-transparent background */}
-    <div className="fixed inset-0 w-screen h-screen bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl text-center text-red-500 font-bold mb-4">Dietary Preferences</h2>
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          {/* Semi-transparent background */}
+          <div className="fixed inset-0 w-screen h-screen bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
+              <h2 className="text-2xl text-center text-red-500 font-bold mb-4">
+                Dietary Preferences
+              </h2>
 
-        {/* Filters inside the Modal */}
-        <div className="flex flex-col space-y-2 mb-4">
-        <label className="flex items-center">
-  <input
-    type="checkbox"
-    name="Allergies"
-    checked={tempFilter.pescatarian}
-    onChange={handleFilterChange}
-    className="mr-3 w-6 h-6"  // Increase the size of the checkbox
-  />
-  <span className="text-lg font-medium">Allergies</span>  
-</label>
+              {/* Filters inside the Modal */}
+              <div className="flex flex-col space-y-2 mb-4">
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="Allergies"
+                    checked={tempFilter.pescatarian}
+                    onChange={handleFilterChange}
+                    className="mr-3 w-6 h-6" // Increase the size of the checkbox
+                  />
+                  <span className="text-lg font-medium">Allergies</span>
+                </label>
 
-          <label className="flex items-center">
-  <input
-    type="checkbox"
-    name="pescatarian"
-    checked={tempFilter.pescatarian}
-    onChange={handleFilterChange}
-    className="mr-3 w-6 h-6"  // Increase the size of the checkbox
-  />
-  <span className="text-lg font-medium">Dairy-free</span> 
-</label>
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="pescatarian"
+                    checked={tempFilter.pescatarian}
+                    onChange={handleFilterChange}
+                    className="mr-3 w-6 h-6" // Increase the size of the checkbox
+                  />
+                  <span className="text-lg font-medium">Dairy-free</span>
+                </label>
 
-<label className="flex items-center">
-  <input
-    type="checkbox"
-    name="Gluten-free"
-    checked={tempFilter.glutenFree}
-    onChange={handleFilterChange}
-    className="mr-3 w-6 h-6"  // Increase the size of the checkbox
-  />
-  <span className="text-lg font-medium">Gluten-free</span> 
-</label>
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="Gluten-free"
+                    checked={tempFilter.glutenFree}
+                    onChange={handleFilterChange}
+                    className="mr-3 w-6 h-6" // Increase the size of the checkbox
+                  />
+                  <span className="text-lg font-medium">Gluten-free</span>
+                </label>
 
-          <label className="flex items-center">
-            <input
-              type="checkbox"
-              name="Vegetarian"
-              checked={tempFilter.pescatarian}
-              onChange={handleFilterChange}
-              className="mr-3 w-6 h-6"
-            />
-            <span className="text-lg font-medium">Lactose-free</span>
-          </label>
-          <label className="flex items-center">
-            <input
-              type="checkbox"
-              name="pescatarian"
-              checked={tempFilter.pescatarian}
-              onChange={handleFilterChange}
-              className="mr-3 w-6 h-6"
-            />
-            <span className="text-lg font-medium">Pescatarian</span>
-          </label>
-          <label className="flex items-center">
-            <input
-              type="checkbox"
-              name="pescatarian"
-              checked={tempFilter.pescatarian}
-              onChange={handleFilterChange}
-              className="mr-3 w-6 h-6"
-            />
-            <span className="text-lg font-medium">Vegan</span>
-          </label>
-          <label className="flex items-center">
-            <input
-              type="checkbox"
-              name="pescatarian"
-              checked={tempFilter.veg}
-              onChange={handleFilterChange}
-              className="mr-3 w-6 h-6"
-            />
-            <span className="text-lg font-medium">Vegetarian</span>
-          </label>
-        </div>
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="Vegetarian"
+                    checked={tempFilter.pescatarian}
+                    onChange={handleFilterChange}
+                    className="mr-3 w-6 h-6"
+                  />
+                  <span className="text-lg font-medium">Lactose-free</span>
+                </label>
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="pescatarian"
+                    checked={tempFilter.pescatarian}
+                    onChange={handleFilterChange}
+                    className="mr-3 w-6 h-6"
+                  />
+                  <span className="text-lg font-medium">Pescatarian</span>
+                </label>
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="pescatarian"
+                    checked={tempFilter.pescatarian}
+                    onChange={handleFilterChange}
+                    className="mr-3 w-6 h-6"
+                  />
+                  <span className="text-lg font-medium">Vegan</span>
+                </label>
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="pescatarian"
+                    checked={tempFilter.veg}
+                    onChange={handleFilterChange}
+                    className="mr-3 w-6 h-6"
+                  />
+                  <span className="text-lg font-medium">Vegetarian</span>
+                </label>
+              </div>
 
-        {/* Confirm and Close buttons */}
-        <div className="flex justify-center space-x-4 mt-6">
-          <CustomButton
-            bgColor="bg-red-600"
-            textColor="text-white"
-            text="Confirm"
-            onClick={() => applyFilters()}
-          />
-          <CustomButton
-            bgColor="bg-gray-700"
-            textColor="text-white"
-            text="Close"
-            onClick={() => closeModal()}
-          />
-        </div>
-      </div>
-    </div>
-  </Modal>
-)}
-
+              {/* Confirm and Close buttons */}
+              <div className="flex justify-center space-x-4 mt-6">
+                <CustomButton
+                  bgColor="bg-red-600"
+                  textColor="text-white"
+                  text="Confirm"
+                  onClick={() => applyFilters()}
+                />
+                <CustomButton
+                  bgColor="bg-gray-700"
+                  textColor="text-white"
+                  text="Close"
+                  onClick={() => closeModal()}
+                />
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
 
       {/* Displaying filtered food items */}
       {showFoodList && (
